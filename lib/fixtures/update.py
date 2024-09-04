5ab70eafb9a28c1f02fd842d97ef030586e11ea3
adisbladis <adisbladis@gmail.com>
2024-09-04 16:53:45 +1200

    Add test fixture upgrade script
    uv.lock is a moving target, and we want to make sure we move with it.
    This test script uses the `--exclude-newer` option of uv to ensure we're locking the same dependencies, just with a newer version of uv.

---
+++ lib/fixtures/update.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import subprocess
+import os.path
+
+# Update test fixtures using a newer version of uv
+#
+# This will use git to figure out when the lock file was originally created to
+# ensure that the same dependency graph is being locked,
+# just with a newer version of uv.
+
+def find_roots():
+    """Find all project roots with a uv.lock"""
+    for root, _, files in os.walk("."):
+        for filename in files:
+            if filename == "uv.lock":
+                yield root
+
+
+def get_file_creation(path: str) -> str:
+    proc = subprocess.run([
+        "git", "log", "--follow", "--format=%ad", "--date", "iso-strict", path
+    ], check=True, stdout=subprocess.PIPE)
+    return proc.stdout.decode().split("\n")[0]
+
+
+def lock(root: str, exclude_newer: str):
+    subprocess.run(["uv", "lock", "--exclude-newer", exclude_newer], check=True, cwd=root)
+
+
+if __name__ == "__main__":
+    for root in find_roots():
+        print(f"Updating lock for: {root}")
+        exclude_newer = get_file_creation(os.path.join(root, "uv.lock"))
+        lock(root, exclude_newer)
