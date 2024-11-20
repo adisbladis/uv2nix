2eef242f03897bbe1d9a349e2ad634069a966955
adisbladis <adisbladis@gmail.com>
2024-11-20 07:59:51 +0000

    dev: Format

--- dev/build-systems.nix
+++ dev/build-systems.nix
@@ -68,49 +68,49 @@ lib.mapAttrs
       };
 
     scikit-build-core =
-{
-  stdenv,
-  lib,
-  python,
-  python3Packages,
-  pyprojectHook,
-  resolveBuildSystem,
-}:
-stdenv.mkDerivation {
-  inherit (python3Packages.scikit-build-core)
-    pname
-    version
-    src
-    meta
-    patches
-    ;
+      {
+        stdenv,
+        lib,
+        python,
+        python3Packages,
+        pyprojectHook,
+        resolveBuildSystem,
+      }:
+      stdenv.mkDerivation {
+        inherit (python3Packages.scikit-build-core)
+          pname
+          version
+          src
+          meta
+          patches
+          ;
 
-  passthru.dependencies =
-    {
-      packaging = [ ];
-      pathspec = [ ];
-    }
-    // lib.optionalAttrs (python.pythonOlder "3.11") {
-      exceptiongroup = [ ];
-      tomli = [ ];
-    }
-    // lib.optionalAttrs (python.pythonOlder "3.9") {
-      importlib-resources = [ ];
-      typing-extensions = [ ];
-    }
-    // lib.optionalAttrs (python.pythonOlder "3.8") {
-      importlib-metadata = [ ];
-    };
+        passthru.dependencies =
+          {
+            packaging = [ ];
+            pathspec = [ ];
+          }
+          // lib.optionalAttrs (python.pythonOlder "3.11") {
+            exceptiongroup = [ ];
+            tomli = [ ];
+          }
+          // lib.optionalAttrs (python.pythonOlder "3.9") {
+            importlib-resources = [ ];
+            typing-extensions = [ ];
+          }
+          // lib.optionalAttrs (python.pythonOlder "3.8") {
+            importlib-metadata = [ ];
+          };
 
-  nativeBuildInputs =
-    [
-      pyprojectHook
-    ]
-    ++ resolveBuildSystem {
-      hatchling = [ ];
-      hatch-vcs = [ ];
-    };
-};
+        nativeBuildInputs =
+          [
+            pyprojectHook
+          ]
+          ++ resolveBuildSystem {
+            hatchling = [ ];
+            hatch-vcs = [ ];
+          };
+      };
 
     ninja =
       {
